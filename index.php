34d1ad3057c290f052aa04b83eeedc82315cc7bf
Vitor Bersano Sacuma <ra2457001>
2025-03-13 10:36:54 -0300

    segundo commit

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php 
-    echo "fortnite";
+    $cidade = "Garça";
+    $dia = 17;
+    $mes = "Julho";
+    $ano = 2022;
+    while($dia<31){
+        echo "<i>$cidade</i>, $dia de <strong>$mes</strong> de <em>$ano</em><br>";
+        
+        $dia +=7;
+    }
 ?>
